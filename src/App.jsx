8ffbb9057ff26a42fc981d9fdfbc16d5b9312f04
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-04-08 00:00:19 -0700

    Main file

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './Header.jsx'
+import Home from './Home.jsx'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    // BEM
+    <>
+      <div>
+        <Header />
+
+        <Home />
+      </div>
+    </>
+  )
+}
+
+export default App
