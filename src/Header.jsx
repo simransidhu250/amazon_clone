720c345607588bd2718044e527899ebb419a66c0
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-04-08 21:43:02 -0700

    Header Styling

---
+++ src/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src='https://www.hatchwise.com/wp-content/uploads/2022/05/amazon-logo-1024x683.png' className='header__logo'></img>
+            <div className="header__search">
+                <input className='header__searchInput' type='text'></input>
+                <SearchIcon className='header__searchIcon' />
+            </div>
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="optionLineOne">Hello</span>
+                    <span className="optionLineTwo">Sign in</span>
+                </div>
+                <div className="header__option">
+                    <span className="optionLineOne">Returns</span>
+                    <span className="optionLineTwo">&Orders</span>
+                </div>
+                <div className="header__option">
+                    <span className="optionLineOne">Your</span>
+                    <span className="optionLineTwo">Prime</span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__basketCount optionLineTwo">0</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
+
+
