40528d0ebd620c5ed9c10e00f7125955da14e747
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-04-10 23:31:58 -0700

    Adding props

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,18 +8,22 @@ function Home() {
             <div className="home__container">
                 <img className="home__image" src="https://store-images.s-microsoft.com/image/apps.24594.14606951565968665.3ee1b191-70b1-4906-a2af-cd057bdef580.c3e81551-5bc2-45ab-9892-204e5c16c034?mode=scale&q=90&h=720&w=1280" alt="" />
                 <div className="home__row">
-                    <Product />
+                    <Product title="Zero To One"
+                        price={29.99}
+                        image="https://m.media-amazon.com/images/I/51zGCdRQXOL._SL1200_.jpg"
+                        rating={2}
+                    />
                     <Product />
                     <Product />
                     <Product />
                 </div>
                 <div className="home__row">
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
+                    <Product />
+                    <Product />
+                    <Product />
                 </div>
                 <div className="home__row">
-                    {/* Product */}
+                    <Product />
                 </div>
             </div>
 
