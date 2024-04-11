607e18172e24ddccb13ce3948f776167b0f2efbe
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-04-10 22:10:18 -0700

    Adding multiple products

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,7 +9,9 @@ function Home() {
                 <img className="home__image" src="https://store-images.s-microsoft.com/image/apps.24594.14606951565968665.3ee1b191-70b1-4906-a2af-cd057bdef580.c3e81551-5bc2-45ab-9892-204e5c16c034?mode=scale&q=90&h=720&w=1280" alt="" />
                 <div className="home__row">
                     <Product />
-                    {/*Product  */}
+                    <Product />
+                    <Product />
+                    <Product />
                 </div>
                 <div className="home__row">
                     {/* Product */}
@@ -21,7 +23,7 @@ function Home() {
                 </div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
