2179e338df7d5f2bf5a808b3956ac46a6043c3cf
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-04-12 17:41:48 -0700

    App file

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@
 import './App.css'
 import Header from './Header.jsx'
 import Home from './Home.jsx'
+import {
+  createBrowserRouter,
+  Router,
+  RouterProvider,
+} from "react-router-dom";
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     // BEM
-    <>
+    <Router>
       <div>
         <Header />
 
         <Home />
       </div>
-    </>
+    </Router>
   )
 }
 
