0330fc26da70ad3d2348594cc462061bdedb2fb7
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-04-13 00:26:42 -0700

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 // import { useState } from 'react'
 import './App.css'
+import Checkout from './Checkout.jsx';
 import Header from './Header.jsx'
 import Home from './Home.jsx'
 import {
   createBrowserRouter,
-  Router,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 
 function App() {
   // const [count, setCount] = useState(0)
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Home />}>
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
+    )
+  );
 
   return (
     // BEM
-    <Router>
-      <div>
-        <Header />
-
-        <Home />
-      </div>
-    </Router>
+    <div className='app'>
+      <Header />
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
