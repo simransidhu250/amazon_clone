670f6b36973fee18e44f4c4a437ce97bc42e1a87
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-05-22 20:57:59 -0700

    Subtotal

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Checkout.css"
+import Subtotal from './Subtotal'
+
 function Checkout() {
     return (
         <div className='checkout'>
-            <div className="chekcout__left">
+            <div className="checkout__left">
                 <img src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_NotApproved._TTW_.jpg" alt="" className="checkout__ad" />
                 <div>
                     <h2 className="checkout__title">Your shopping Basket</h2>
@@ -15,7 +17,7 @@ function Checkout() {
             </div>
 
             <div className="checkout__right">
-                <h2>The subtotal will go here</h2>
+                <Subtotal />
             </div>
         </div>
     )
