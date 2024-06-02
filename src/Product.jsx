a383ca6d17098446903f2e77823229081ad2800d
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-06-01 21:28:22 -0700

    Product viewing

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import "./Product.css"
 import StarIcon from '@mui/icons-material/Star';
+import { useStateValue } from './StateProvider';
 
-function Product({ id, title, price, rating, image }) {
+function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+    // console.log("This is the basked", basket);
+
+    const addToBasket = () => {
+        //dispatch the item into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+
     return (
         <div className="product">
             <div className="product__info">
@@ -17,7 +35,7 @@ function Product({ id, title, price, rating, image }) {
             </div>
             <p></p>
             <img src={image} alt="" />
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
 
     )
