d63c6aaecd494be0f9935e0a3b2a68d6096b2856
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-07-01 15:59:52 -0700

    Remove from basket functionality and Subtotal

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import "./Checkout.css"
 import Subtotal from './Subtotal'
+import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className='checkout'>
             <div className="checkout__left">
                 <img src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_NotApproved._TTW_.jpg" alt="" className="checkout__ad" />
                 <div>
                     <h2 className="checkout__title">Your shopping Basket</h2>
-                    {/* <BasketItem /> */}
-                    {/* <BasketItem /> */}
-                    {/* <BasketItem /> */}
-                    {/* <BasketItem /> */}
+                    <h2>{basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating} />
+
+                    ))}</h2>
                 </div>
             </div>
 
