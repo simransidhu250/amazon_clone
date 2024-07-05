11a414b4ba85de38c728d1f7a311442ef14f81b0
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-07-04 23:41:15 -0700

    Showing up user name

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -6,12 +6,14 @@ import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
 
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
         <div className='checkout'>
+
             <div className="checkout__left">
                 <img src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/ILB_BrightColors_NotApproved._TTW_.jpg" alt="" className="checkout__ad" />
                 <div>
+                    <h3>{user.email}</h3>
                     <h2 className="checkout__title">Your shopping Basket</h2>
                     <h2>{basket.map(item => (
                         <CheckoutProduct
