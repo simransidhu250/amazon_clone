5ea48ff12640982231942849f695ee62bea6270a
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-07-14 18:27:57 -0700

    StateProvider

---
+++ src/StateProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+//Prepares the dataLayer
+export const StateContext = createContext();
+
+//Wrap our app and provide the Data layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+);
+
+
+//Pull information from the data layer
+export const useStateValue = () => {
+    const context = useContext(StateContext);
+    if (context === undefined) {
+        throw new Error('useStateValue must be used within a StateProvider');
+    }
+    return context;
+};
