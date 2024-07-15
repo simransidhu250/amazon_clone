c164f6876458253c755e455191f839fe787ad27b
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-07-14 18:27:22 -0700

    proceed to checkout button

--- src/Subtotal.jsx
+++ src/Subtotal.jsx
@@ -3,9 +3,12 @@ import CurrencyFormat from 'react-currency-format'
 import "./Subtotal.css"
 import { useStateValue } from './StateProvider'
 import { getBasketTotal } from './reducer';
+import { useNavigate } from 'react-router-dom'
+
 
 function Subtotal() {
     const [{ basket }, dispatch] = useStateValue();
+    const navigate = useNavigate();
     return (
         <div className='subtotal'>
             <CurrencyFormat
@@ -27,7 +30,7 @@ function Subtotal() {
                 prefix={"$"}
             />
 
-            <button>Proceed to Checkout</button>
+            <button onClick={() => navigate('/payment')}>Proceed to Checkout</button>
         </div>
     )
 }
