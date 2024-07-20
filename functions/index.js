d408d65d71d160b5edec9d94d54c286126ad47f4
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2024-07-19 23:35:09 -0700

    index

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// contains secret key
+import { onRequest } from "firebase-functions/v2/https";
+import logger from "firebase-functions/logger";
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const express = require("express")
-const cors = require("cors")
-const stripe = require("stripe")('secret key goes here')
+import express, { json } from "express";
+import cors from "cors";
+const stripe = require("stripe")('')
 
 // API
 
@@ -28,7 +13,7 @@ const stripe = require("stripe")('secret key goes here')
 const app = express();
 // App config
 app.use(cors({ origin: true }));
-app.use(express.json());
+app.use(json());
 
 // API routes
 app.get('/', (request, response) => response.status(200).send('hello world'));
@@ -37,7 +22,7 @@ app.post('/payments/create', async (request, response) => {
     const total = request.query.total;
     console.log('Payment Request Received BOOM!!! for this amount >>>', total)
 
-    console.paymentIntent = await stripe.paymentIntent.create({
+    const paymentIntent = await stripe.paymentIntents.create({
         amount: total,  //subunits of the currency
         currency: "usd",
     });
@@ -48,8 +33,7 @@ app.post('/payments/create', async (request, response) => {
     })
 })
 
-// Listen command
-exports.api = onRequest(app)
+export const api = onRequest(app)
 
 // Example endpoint
 // http://127.0.0.1:5001/clone-609bb/us-central1/api
